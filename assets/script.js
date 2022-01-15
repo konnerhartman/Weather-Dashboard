8105beb90e9f631749897a9309cdcb7a583e079e
Konner Hartman <konner.hartman@yahoo.com>
2022-01-15 09:42:58 -0600

    added variables to script.js and edited index.html

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const APIKey = "934eb5d41c7e9486a38e0587aac82ebf";
+const searchCity = document.querySelector("#searchCity");
+const searchBtn = document.querySelector("#searchBtn");
+const currentCity = document.querySelector(".currentCity");
+const fiveDay = document.querySelector(".fiveDay");
