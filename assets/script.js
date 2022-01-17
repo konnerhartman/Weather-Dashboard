4511893f880e12d4f7f6a91dc1c19f5ddfdd5eac
Konner Hartman <konner.hartman@yahoo.com>
2022-01-16 20:19:26 -0600

    started on functions

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,23 @@ const searchCity = document.querySelector("#searchCity");
 const searchBtn = document.querySelector("#searchBtn");
 const currentCity = document.querySelector(".currentCity");
 const fiveDay = document.querySelector(".fiveDay");
+
+let currentWeather = (city) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`, {
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow'
+    })
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) => {
+        let getData = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&appid=${weatherAPIKey}&units=imperial`
+
+        fetch(getData)
+        .then((response) => {
+            return response.json()
+    })
+}
