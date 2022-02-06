e8951b8e1170a724fb37612d8388e328e4da065f
Konner Hartman <konner.hartman@yahoo.com>
2022-02-06 12:16:36 -0600

    added a little js

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,29 @@
 const APIKey = "934eb5d41c7e9486a38e0587aac82ebf";
 const searchCity = document.querySelector("#searchCity");
-const searchBtn = document.querySelector("#searchBtn");
+const searchBtn = document.getElementById("#searchBtn");
 const currentCity = document.querySelector(".currentCity");
 const fiveDay = document.querySelector(".fiveDay");
 
-let currentWeather = (city) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`, {
-        method: 'GET',
-        credentials: 'same-origin',
-        redirect: 'follow'
-    })
-
-    .then((response) => {
-        return response.json()
-    })
-
-    .then((data) => {
-        let getData = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude={part}&appid=${weatherAPIKey}&units=imperial`
-
-        fetch(getData)
-        .then((response) => {
-            return response.json()
-    })
+let weatherApp = {
+    fetchWeather: (searchCity) => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${APIKey}&units=imperial`, {
+            method: 'GET',
+            credentials: 'same-origin',
+            redirect: 'follow'
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then((data) => this.renderWeather(data));
+    },
+        
+    renderWeather: (data) => {
+        const { name } = (data);
+        const { icon, description } = data.weather;
+        const { temp, humidty } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidty, speed);
+    }
 }
+
+// searchBtn.addEventListener('click', fetchWeather());
